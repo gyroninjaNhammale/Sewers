806d720a8fbfcaa07e405159c696f2b7372dc2f9
gyroninja <gyroninja@gmail.com>
2011-12-19 16:34:22 -0800

    Tunnels mostly all done and look pretty good just need to work on the path a little.

--- Sewers/src/me/hammale/Sewer/UltraGen.java
+++ Sewers/src/me/hammale/Sewer/UltraGen.java
@@ -27,9 +27,10 @@ public class UltraGen {
 		BlockFace bf6 = BlockFace.DOWN;
 
 		int dir = 1;
+		int olddir = 1;
 		int rooms1 = rooms - 2;
 
-		Block set1 = set.getRelative(BlockFace.EAST, 1);
+		Block set1 = set.getRelative(bf3, 1);
 
 		int man1 = man.man2(set1, m, bf2);
 		
@@ -51,6 +52,9 @@ public class UltraGen {
 		Block set9 = set8.getRelative(bf6, 1);
 		Block pos = set9.getRelative(bf3, 3);
 
+		Block set10;
+		Block set11;
+
 		for (int roomcount = 2; roomcount <= rooms1; roomcount++) {
 
 			if (roomcount != 2) {
@@ -58,42 +62,137 @@ public class UltraGen {
 				dir = nextdir(dir);
 			}
 
-			System.out.println(roomcount);
+			System.out.println(roomcount - 1);
+			System.out.println(dir);
 
 			if (dir == 0) {
 
-				int tun2 = tun.nstStraight(pos, m, bf1);
+				if (olddir == 2 || olddir == 3) {
 
-				set8 = set7.getRelative(bf1, tun2 - 3);
-				set9 = set8.getRelative(bf6, 1);
-				pos = set9.getRelative(bf4, 3);
+					set10 = pos.getRelative(bf1, 2);
+
+					int tun2 = tun.nstStraight(set10, m, bf1);
+
+					pos = set10.getRelative(bf1, tun2 - 3);
+
+					olddir = 0;
+				}
+
+				else {
+
+					int tun2 = tun.nstStraight(pos, m, bf1);
+
+					pos = pos.getRelative(bf1, tun2 - 3);
+
+					olddir = 0;
+				}
 			}
 
 			if (dir == 1) {
 
-				int tun3 = tun.nstStraight(pos, m, bf2);
+				if (olddir == 2 || olddir == 3) {
 
-				set8 = set7.getRelative(bf2, tun3 - 3);
-				set9 = set8.getRelative(bf6, 1);
-				pos = set9.getRelative(bf3, 3);
+					set10 = pos.getRelative(bf2, 2);
+
+					int tun3 = tun.nstStraight(set10, m, bf2);
+
+					pos = set10.getRelative(bf2, tun3 - 3);
+
+					olddir = 1;
+				}
+
+				else {
+
+					int tun3 = tun.nstStraight(pos, m, bf2);
+
+					pos = pos.getRelative(bf2, tun3 - 3);
+
+					olddir = 1;
+				}
 			}
 
 			if (dir == 2) {
 
-				int tun4 = tun.ewtStraight(pos, m, bf3);
+				if (olddir == 0) {
 
-				set8 = set7.getRelative(bf4, tun4 - 3);
-				set9 = set8.getRelative(bf6, 1);
-				pos = set9.getRelative(bf2, 3);
+					System.out.println(":D 0");
+
+					set10 = pos.getRelative(bf3, 2);
+
+					set11 = set10.getRelative(bf2, 1);
+
+					int tun4 = tun.ewtStraight(set11, m, bf3);
+
+					pos = set11.getRelative(bf3, tun4 - 4);
+
+					olddir = 2;
+				}
+
+				if (olddir == 1) {
+
+					System.out.println(":D 0");
+
+					set10 = pos.getRelative(bf3, 2);
+
+					set11 = set10.getRelative(bf1, 1);
+
+					int tun4 = tun.ewtStraight(set11, m, bf3);
+
+					pos = set11.getRelative(bf3, tun4 - 4);
+
+					olddir = 2;
+				}
+
+				else {
+
+					set10 = pos.getRelative(bf3, 2);
+
+					int tun4 = tun.ewtStraight(set10, m, bf3);
+
+					pos = set10.getRelative(bf3, tun4 - 4);
+
+					olddir = 2;
+				}
 			}
 
 			if (dir == 3) {
 
-				int tun5 = tun.ewtStraight(pos, m, bf3);
+				if (olddir == 0) {
 
-				set8 = set7.getRelative(bf3, tun5 - 3);
-				set9 = set8.getRelative(bf6, 1);
-				pos = set9.getRelative(bf1, 3);
+					set10 = pos.getRelative(bf4, 2);
+
+					set11 = set10.getRelative(bf2, 1);
+
+					int tun5 = tun.ewtStraight(set11, m, bf4);
+
+					pos = set11.getRelative(bf4, tun5 - 4);
+
+					olddir = 3;
+				}
+
+				if (olddir == 1) {
+
+					set10 = pos.getRelative(bf4, 2);
+
+					set11 = set10.getRelative(bf1, 1);
+
+					int tun5 = tun.ewtStraight(set11, m, bf4);
+
+					pos = set11.getRelative(bf4, tun5 - 4);
+
+					olddir = 3;
+				}
+
+				else {
+
+					set10 = pos.getRelative(bf4, 2);
+
+					int tun5 = tun.ewtStraight(set10, m, bf4);
+
+					pos = set10.getRelative(bf4, tun5 - 4);
+
+					olddir = 3;
+				}
 			}
 		}
 
@@ -101,7 +200,7 @@ public class UltraGen {
 
 	int nextdir (int dir) {
 
-		int ran = gen.nextInt(8);
+		int ran = gen.nextInt(5);
 		
 		if (dir == 0) {
 
@@ -122,30 +221,10 @@ public class UltraGen {
 
 			if (ran == 3) {
 
-				dir = 0;
-			}
-
-			if (ran == 4) {
-
-				dir = 0;
-			}
-
-			if (ran == 5) {
-
-				dir = 0;
-			}
-
-			if (ran == 6) {
-
-				dir = 0;
-			}
-
-			if (ran == 7) {
-
 				dir = 2;
 			}
 
-			if (ran == 8) {
+			if (ran == 4) {
 
 				dir = 3;
 			}
@@ -171,30 +250,10 @@ public class UltraGen {
 
 			if (ran == 3) {
 
-				dir = 1;
-			}
-
-			if (ran == 4) {
-
-				dir = 1;
-			}
-
-			if (ran == 5) {
-
-				dir = 1;
-			}
-
-			if (ran == 6) {
-
-				dir = 1;
-			}
-
-			if (ran == 7) {
-
 				dir = 2;
 			}
 
-			if (ran == 8) {
+			if (ran == 4) {
 
 				dir = 3;
 			}
@@ -226,26 +285,6 @@ public class UltraGen {
 
 				dir = 2;
 			}
-
-			if (ran == 5) {
-
-				dir = 2;
-			}
-
-			if (ran == 6) {
-
-				dir = 2;
-			}
-
-			if (ran == 7) {
-
-				dir = 2;
-			}
-
-			if (ran == 8) {
-
-				dir = 2;
-			}
 		}
 
 		if (dir == 3) {
@@ -271,26 +310,6 @@ public class UltraGen {
 			}
 
 			if (ran == 4) {
-
-				dir = 3;
-			}
-
-			if (ran == 5) {
-
-				dir = 3;
-			}
-
-			if (ran == 6) {
-
-				dir = 3;
-			}
-
-			if (ran == 7) {
-
-				dir = 3;
-			}
-
-			if (ran == 8) {
 
 				dir = 3;
 			}
