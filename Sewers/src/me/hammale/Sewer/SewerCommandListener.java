c88704f5408d27e7cc1c1c0054cb5223318ca5b7
gyroninja <gyroninja@gmail.com>
2011-12-24 13:25:43 -0800

    The code is there but <size> is not working,

--- Sewers/src/me/hammale/Sewer/SewerCommandListener.java
+++ Sewers/src/me/hammale/Sewer/SewerCommandListener.java
@@ -16,16 +16,22 @@ import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class SewerCommandListener implements CommandExecutor {
     
-	private final SewerGenerator sewergen = new SewerGenerator();
+	private final UltraGen ugen = new UltraGen();
 	
-	private final SewerLocate loc = new SewerLocate();
-		
-    public final Sewer plugin;
+    public Sewer plugin;
+
+    Random gen = new Random();
+
+    String arg;
+    String arg1;
+    int a1;
     
-	public SewerCommandListener(Sewer plugin) {
+    public SewerCommandListener(Sewer plugin) {
 		this.plugin = plugin;
 	}
 	
+	private final SewerLocate loc = new SewerLocate();
+		
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		
@@ -37,31 +43,75 @@ public class SewerCommandListener implements CommandExecutor {
 		if (command.getName().equalsIgnoreCase("sewer")) {
 			
 			if(args.length == 1){
-			String arg = args[0];
-			if(arg.equalsIgnoreCase("create")){		
-			if (p == null) {
-				sender.sendMessage("[Sewers] This command can only be run by a player!");
-			} else if (p.isOp()) {
-				p.sendMessage(ChatColor.GREEN + "Generating Sewer...");
-				Block b = p.getLocation().getBlock();
-				b.setType(Material.AIR);
-				Material m = (Material.SMOOTH_BRICK);
-				BlockFace bf = BlockFace.NORTH;
-				Block set2 = b;
-				//sewergen.start1(set2, m, bf);
-				sewergen.apreview(set2, m, bf);
-				//nest.nest1(set2, m, bf);
-				//bridge.nsbridge(set2, m, bf);
-				//cavein.cavein1(set2, m, bf);
-				//grave.grave(set2, m, bf);
-				p.sendMessage(ChatColor.GREEN + "Sewer Complete!");
+				arg = args[0];
+				if(arg.equalsIgnoreCase("create")){
+					if (p == null) {
+						sender.sendMessage("[Sewers] This command can only be run by a player!");
+					} else if (p.isOp()) {
+						p.sendMessage(ChatColor.GREEN + "Generating Sewer...");
+						Block b = p.getLocation().getBlock();
+						b.setType(Material.AIR);
+						Material m = (Material.SMOOTH_BRICK);
+						BlockFace bf = BlockFace.NORTH;
+						Block set2 = b;
+						int roomnum = 0;
+						while(roomnum < 50) {
+							roomnum = gen.nextInt(100);
+						}
+						ugen.ugen(set2, m, bf, roomnum);
+						//sewergen.start1(set2, m, bf);
+						//sewergen.apreview(set2, m, bf);
+						//nest.nest1(set2, m, bf);
+						//bridge.nsbridge(set2, m, bf);
+						//cavein.cavein1(set2, m, bf);
+						//grave.grave(set2, m, bf);
+						p.sendMessage(ChatColor.GREEN + "Sewer Complete!");
 										
-			return true;
+						return true;
+					}
+					else {
+						sender.sendMessage("This command can only be run by an OP!");
+					}
+				}
+				
+			if(args.length == 2){
+				arg = args[0];
+				arg1 = args[1];
+
+				if(arg.equalsIgnoreCase("create")){
+	 				if (p == null) {
+	 					sender.sendMessage("[Sewers] This command can only be run by a player!");
+					} else if (p.isOp()) {
+						try {
+							a1 = Integer.parseInt(arg1);
+						}
+						catch(NumberFormatException e) {
+							e.printStackTrace();
+							sender.sendMessage(arg1 + "is not a number");
+							return false;
+						}
+						p.sendMessage(ChatColor.GREEN + "Generating Sewer...");
+						Block b = p.getLocation().getBlock();
+						b.setType(Material.AIR);
+						Material m = (Material.SMOOTH_BRICK);
+						BlockFace bf = BlockFace.NORTH;
+						Block set2 = b;
+						ugen.ugen(set2, m, bf, a1);
+						//sewergen.start1(set2, m, bf);
+						//sewergen.apreview(set2, m, bf);
+						//nest.nest1(set2, m, bf);
+						//bridge.nsbridge(set2, m, bf);
+						//cavein.cavein1(set2, m, bf);
+						//grave.grave(set2, m, bf);
+						p.sendMessage(ChatColor.GREEN + "Sewer Complete!");
+				
+						return true;
+					}
+					else {
+						sender.sendMessage("This command can only be run by an OP!");
+					}
+				}
 			}
-			else {
-				sender.sendMessage("This command can only be run by an OP!");
-			}
-		}
 			
 			if(arg.equalsIgnoreCase("locate")){
 				if (p == null) {
@@ -71,7 +121,7 @@ public class SewerCommandListener implements CommandExecutor {
 					
 					Location l = loc.findSewer(p.getWorld(), p);
 					if(l != null){
-					p.sendMessage(ChatColor.GREEN + "Closest sewer located @: " + ChatColor.BLUE + l.getX()+ ChatColor.GREEN + ", " + ChatColor.YELLOW + l.getY()+ ChatColor.GREEN + ", " + ChatColor.RED + l.getZ());				
+						p.sendMessage(ChatColor.GREEN + "Closest sewer located @: " + ChatColor.BLUE + l.getX()+ ChatColor.GREEN + ", " + ChatColor.YELLOW + l.getY()+ ChatColor.GREEN + ", " + ChatColor.RED + l.getZ());				
 					}else{
 						p.sendMessage(ChatColor.RED + "No Sewer's found!");
 					}
@@ -82,55 +132,36 @@ public class SewerCommandListener implements CommandExecutor {
 				}
 			}
 			
-			if(arg.equalsIgnoreCase("nav")){
-				if (p == null) {
-					sender.sendMessage("This command can only be run by a player!");
-				} else if (p.isOp()) {
-					SpoutPlayer player = (SpoutPlayer) p;
-					if(player.isSpoutCraftEnabled()){
-					if(loc.nav == true){
-						
-						if(!(loc.active.isEmpty() || loc.active.contains(p.getName()))){						
-							sender.sendMessage(ChatColor.RED + "OH NOES! It appears that " + loc.active + " is already navigating! One at a time please!");
-							return true;						
-						}
-						
-						sender.sendMessage(ChatColor.RED + "Stopping Sewer navigation...");
-						loc.StopNav(p);
-					}else{
-						if(!(loc.active.isEmpty())){						
-							sender.sendMessage(ChatColor.RED + "OH NOES! It appears that " + loc.active + " is already navigating! One at a time please!");
-							return true;						
-						}
-						sender.sendMessage(ChatColor.GREEN + "Starting Sewer navigation...");
-						sender.sendMessage(ChatColor.GREEN + "To stop navigation type '/sewer nav'");
-						loc.DisplayArrows(p);				
-					}					
-				return true;
-					}else{
-						sender.sendMessage(ChatColor.RED + "OH NOES! You aren't using SpoutCraft so navigation is impossiable!");
-						return true;
-					}
-				}
-				else {
-					sender.sendMessage("This command can only be run by an OP!");
-				}
-			}
-			
-			if(arg.equalsIgnoreCase("reload")){
-				if (p.isOp()) {
-					plugin.reloadConfig();
-					sender.sendMessage(ChatColor.GREEN + "Sewers Reloaded!");
-					return true;
-				}else {
-					sender.sendMessage("This command can only be run by an OP!");
-				}
-			}
-			
+//			if(arg.equalsIgnoreCase("nav")){
+//				if (p == null) {
+//					sender.sendMessage("This command can only be run by a player!");
+//				} else if (p.isOp()) {
+//					SpoutPlayer player = (SpoutPlayer) p;
+//					if(player.isSpoutCraftEnabled()){
+//						if(loc.nav == true){
+//							sender.sendMessage(ChatColor.RED + "Stopping Sewer navigation...");
+//							loc.StopNav(p);
+//						}else{
+//							if(!(loc.active.isEmpty())){
+//							
+//								sender.sendMessage(ChatColor.RED + "OH NOES! It appears that " + loc.active + " is already navigating! One at a time please!");
+//							
+//							}
+//							sender.sendMessage(ChatColor.GREEN + "Starting Sewer navigation...");
+//							sender.sendMessage(ChatColor.GREEN + "To stop navigation type '/sewer nav'");
+//							loc.DisplayArrows(p);				
+//						}					
+//						return true;
+//					}else{
+//						sender.sendMessage(ChatColor.RED + "OH NOES! You aren't using SpoutCraft so navigation is impossiable!");
+//					}
+//				}
+//				else {
+//					sender.sendMessage("This command can only be run by an OP!");
+//				}
+//			}
+		}
 	}
-}
-		return false;
+return false;
 }		
-
-
 }
